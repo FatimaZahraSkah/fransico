3acf47dbaee4159d64cd8b297e352b94f42d3a66
oussama-ca <choussama36@gmail.com>
2023-02-28 15:40:54 +0100

    from oussama

---
+++ index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>from oussama</h1>
+    <ul>
+        <li><a href="#">oussama</a></li>
+        <li>amezian</li>
+        <li>fatima</li>
+        <li><a href="ali.html">ali</a></li>
+    </ul>
+</body>
+</html>
